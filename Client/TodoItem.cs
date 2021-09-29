427fbd1d606961d7c44c6ad7844d626856cec679
Oscar Fox <oefox0055@eagle.fgcu.edu>
2021-09-29 12:55:37 -0400

    Todo List

---
+++ Client/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
